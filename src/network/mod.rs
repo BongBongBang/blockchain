aae1e332eb9d10158d76ea1d9f657974ed7b7c19
aaron <343129625@qq.com>
2025-09-23 00:12:44 +0800

    feature: frame encoder \ decoder

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -2,9 +2,13 @@ pub mod command;
 
 use std::sync::{LazyLock, OnceLock};
 
+use bytes::BytesMut;
 use tokio::{
-    io::AsyncWriteExt, net::{TcpListener, TcpSocket, TcpStream}, sync::RwLock
+    io::{self, AsyncWriteExt},
+    net::{TcpListener, TcpSocket, TcpStream},
+    sync::RwLock,
 };
+use tokio_util::codec::{Decoder, Encoder};
 
 use crate::network::command::Command;
 
@@ -16,36 +20,63 @@ use crate::network::command::Command;
 struct Server {
     pub node_id: u32,
     pub miner_address: String,
-    pub konwn_hosts: Vec<String>,
+    pub known_hosts: Vec<String>,
 }
 
 impl Server {
+    pub fn new(node_id: u32, miner_address: String) -> Self {
+        Self {
+            node_id,
+            miner_address,
+            known_hosts: vec![String::from("localhost:3000")],
+        }
+    }
+
     /// start a blockchain node
     ///
     /// # Arguments
     ///
     /// - `node_id` (`u32`) - node_id
     /// - `miner_address` (`String`) - miner address
-    pub async fn start_node(&self, node_id: u32, miner_address: String) {
-        // init local node info
-        // NODE_ID.set(node_id);
-        // MINER_ADDRESS.set(miner_address);
+    pub async fn start_node(&self) {
 
-        // // start node server
-        // let addr = format!("localhost:{}", node_id);
-        // let listener = TcpListener::bind(&addr).await.unwrap();
+        // start node server
+        let addr = format!("localhost:{}", self.node_id);
+        let listener = TcpListener::bind(&addr).await.unwrap();
 
-        // // sync version to center node
-        // if &addr != KNOWN_HOSTS.read().await.get(0).unwrap() {
-        //     // send_version();
-        // }
+        // sync version to center node
+        if &addr != self.known_hosts.get(0).unwrap() {
+            // send_version();
+        }
 
-        // // process income
-        // loop {
-        //     let (socket, _) = listener.accept().await.unwrap();
-        // }
+        // process income
+        loop {
+            let (socket, _) = listener.accept().await.unwrap();
+        }
     }
 }
+
+
+pub struct LengthHeaderDelimiter;
+
+impl Decoder for LengthHeaderDelimiter {
+    type Item = Vec<u8>;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        todo!()
+    }
+}
+
+impl Encoder<BytesMut> for LengthHeaderDelimiter {
+    type Error = io::Error;
+
+    fn encode(&mut self, item: BytesMut, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.reserve(additional);
+        todo!()
+    }
+}
+
 
 trait Transmitter {
     async fn transmit<T: Command>(&self, addr: &str, cmd: T);
